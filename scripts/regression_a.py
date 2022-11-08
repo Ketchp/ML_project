d75db6e0f2b17fee069d61a39eb8136370c5e036
AndreaTex99 <andrea.tessarin.3@studenti.unipd.it>
2022-11-08 16:59:59 +0100

    Update regression_a.py

--- scripts/regression_a.py
+++ scripts/regression_a.py
@@ -1,5 +1,6 @@
 from data_preparation import df
 from sklearn.model_selection import KFold
+from sklearn.linear_model import Ridge
 import numpy as np
 
 regression_target = 'age'
@@ -33,11 +34,25 @@ for i, (train_index, test_index) in enumerate(selector.split(X)):
     X_train, y_train = X[train_index, :], y[train_index]
     X_test, y_test = X[test_index, :], y[test_index]
 
+    errors = np.ndarray((K, len(lambda_list)))
+    
     for j, reg_lambda in enumerate(lambda_list):
-        pass
+        
+        lm = Ridge(alpha=1/reg_lambda).fit(X_train, y_train)
+        
+        y_pred = lm.predict(X_test)
+        
+        error = np.mean(ns.power(y_pred - y_test), 2)
+        
         # Train linear regressor with regularisation reg_lambda
         # test model on test values and save error
-
+    
+    errors = np.mean(errors, axis=0)
+    
+    print(errors)
+    
     # Find ideal lambda
     # Train new models with lambda on X_train / or save model for every lambda and use saved model
     # Compute Etest for table on X_test, y_test
+
+    
